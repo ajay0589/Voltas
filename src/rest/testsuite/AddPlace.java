dba5380c79d14c977bc88f33adb0e94e1d63cc62
ajay0589 <ajaylvur@gmail.com>
2020-09-02 21:40:18 +0530

    Rest assured changes

--- src/rest/testsuite/AddPlace.java
+++ src/rest/testsuite/AddPlace.java
@@ -1,24 +1,45 @@
 package rest.testsuite;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+import static org.hamcrest.Matchers.*;
+//import static org.hamcrest.CoreMatchers.*;
 import static io.restassured.RestAssured.*;
 
 public class AddPlace {
 
-	public static void main(String[] args) {
+	ReadTestJson json;
+
+	public void runTest() throws Exception {
+		json = new ReadTestJson();
+		json.readJson();
 
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-		String response = given().log().all().headers("Content-Type", "application/json").headers("key", "qaclick123")
-				.body("{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -93.383494,\r\n" + "    \"lng\": 42.427362\r\n"
-						+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Frontline house\",\r\n"
-						+ "  \"phone_number\": \"(+91) 983 893 1234\",\r\n"
-						+ "  \"address\": \"29, side layout, lattu 09\",\r\n" + "  \"types\": [\r\n"
-						+ "    \"shoe park\",\r\n" + "    \"shop\"\r\n" + "  ],\r\n"
-						+ "  \"website\": \"http://hello.com\",\r\n" + "  \"language\": \"French-IN\",\r\n"
-						+ "  \"language111111\": \"French-IN\"\r\n" + "}")
-				.when().post("maps/api/place/add/json").then().log().all().extract().response().toString();
+		Response response = given().log().all().headers("Content-Type", "application/json").header("key", "qaclick123")
+				.body(json.returnJson()).when().post("maps/api/place/add/json").then().log().all().extract().response();
 
+		int statusCode = response.getStatusCode();
+		if (statusCode == 200) {
+			System.out.println("stattus code passed");
+		} else {
+			System.out.println("stattus code failed");
+		}
+
+		response.then().assertThat().body("status", equalTo("OK"));
+
+		ResponseBody responseBody = response.getBody();
+
+		JsonPath obj = new JsonPath(responseBody.toString());
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		AddPlace obj = new AddPlace();
+		obj.runTest();
 	}
 
 }
